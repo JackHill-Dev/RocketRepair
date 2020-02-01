b9e5819c981f254a0f8ed1d625dede706cee47ef
Jack Parkin <38892053+jpcmr@users.noreply.github.com>
2020-02-01 17:04:15 +0000

    Player Movement, Orbitting objects, Planet model

---
+++ Source/RocketRepair/RocketRepairGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RocketRepairGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROCKETREPAIR_API ARocketRepairGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
